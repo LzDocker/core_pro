797826b87e2bab6c3e0a57bcdfa89333d85e92b8
zhangxindang <zhangxindang@newbeescm.com>
2019-05-18 14:14:44 +0800

    t

--- core/src/main/java/com/docker/core/util/AutoClearedValue.java
+++ core/src/main/java/com/docker/core/util/AutoClearedValue.java
@@ -16,15 +16,15 @@ public class AutoClearedValue<T> {
     private T value;
 
     public AutoClearedValue(final Fragment fragment, T value) {
-        final FragmentManager fragmentManager = fragment.getFragmentManager();
-        fragmentManager.registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
-            public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
-                if(f == fragment) {
-                    AutoClearedValue.this.value = null;
-                    fragmentManager.unregisterFragmentLifecycleCallbacks(this);
-                }
-            }
-        }, false);
+//        final FragmentManager fragmentManager = fragment.getFragmentManager();
+//        fragmentManager.registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
+//            public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
+//                if(f == fragment) {
+//                    AutoClearedValue.this.value = null;
+//                    fragmentManager.unregisterFragmentLifecycleCallbacks(this);
+//                }
+//            }
+//        }, false);
         this.value = value;
     }
 
