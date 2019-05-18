352fd8192a1ccc8691d7c6fcc516783580bc9aad
zhangxindang <zhangxindang@newbeescm.com>
2019-05-18 12:10:11 +0800

    提交

--- core/src/main/java/com/docker/core/base/basehivs/HivsBaseActivity.java
+++ core/src/main/java/com/docker/core/base/basehivs/HivsBaseActivity.java
@@ -4,6 +4,7 @@ import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.docker.core.R;
 import com.docker.core.base.BaseActivity;
 import com.docker.core.util.Empty;
 import com.docker.core.util.ViewEventResouce;
@@ -64,17 +65,29 @@ public abstract class HivsBaseActivity<VM extends HivsBaseViewModel, VB extends 
     @Override
     protected void initImmersionBar() {
 
+//        ImmersionBar.with(this)
+//                .fitsSystemWindows(true)  //使用该属性,必须指定状态栏颜色
+//                .statusBarColor("#ffffff")
+//                .statusBarDarkFont(true)   //状态栏字体是深色，不写默认为亮色
+//                .autoDarkModeEnable(true) //自动状态栏字体和导航栏图标变色，必须指定状态栏颜色和导航栏颜色才可以自动变色哦
+//                .autoStatusBarDarkModeEnable(true, 0.2f) //自动状态栏字体变色，必须指定状态栏颜色才可以自动变色哦
+//                .autoNavigationBarDarkModeEnable(true, 0.2f) //自动导航栏图标变色，必须指定导航栏颜色才可以自动变色哦
+//                .flymeOSStatusBarFontColor("#000000")  //修改flyme OS状态栏字体颜色
+//                .navigationBarColor("#ffffff")
+//                .fullScreen(true)
+//                .addTag("PicAndColor")
+//                .init();
+
         ImmersionBar.with(this)
                 .fitsSystemWindows(true)  //使用该属性,必须指定状态栏颜色
-                .statusBarColor("#ffffff")
-                .statusBarDarkFont(true)   //状态栏字体是深色，不写默认为亮色
+                .statusBarColor(R.color.colorPrimary)
+                .navigationBarColor("#FFFFFF")
+                .fullScreen(true)
+                .navigationBarDarkIcon(true) //导航栏图标是深色，不写默认为亮色
                 .autoDarkModeEnable(true) //自动状态栏字体和导航栏图标变色，必须指定状态栏颜色和导航栏颜色才可以自动变色哦
                 .autoStatusBarDarkModeEnable(true, 0.2f) //自动状态栏字体变色，必须指定状态栏颜色才可以自动变色哦
                 .autoNavigationBarDarkModeEnable(true, 0.2f) //自动导航栏图标变色，必须指定导航栏颜色才可以自动变色哦
                 .flymeOSStatusBarFontColor("#000000")  //修改flyme OS状态栏字体颜色
-                .navigationBarColor("#ffffff")
-                .fullScreen(true)
-                .addTag("PicAndColor")
                 .init();
     }
 
--- core/src/main/java/com/docker/core/util/AutoClearedValue.java
+++ core/src/main/java/com/docker/core/util/AutoClearedValue.java
@@ -1,34 +1,49 @@
 package com.docker.core.util;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
 /**
  * Created by zhangxindang on 2018/8/31.
- *
- *
+ * <p>
+ * <p>
  * fragment 中反注册 databing
- *
  */
 
 public class AutoClearedValue<T> {
 
     private T value;
 
+//    public AutoClearedValue(final Fragment fragment, T value) {
+//        final FragmentManager fragmentManager = fragment.getFragmentManager();
+//        fragmentManager.registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
+//            public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
+//                if(f == fragment) {
+//                    AutoClearedValue.this.value = null;
+//                    fragmentManager.unregisterFragmentLifecycleCallbacks(this);
+//                }
+//            }
+//        }, false);
+//        this.value = value;
+//    }
+
+    public T get() {
+        return this.value;
+    }
+
     public AutoClearedValue(final Fragment fragment, T value) {
-        final FragmentManager fragmentManager = fragment.getFragmentManager();
-        fragmentManager.registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
-            public void onFragmentViewDestroyed(FragmentManager fm, Fragment f) {
-                if(f == fragment) {
+        FragmentManager manager = fragment.getFragmentManager();
+        manager.registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
+            @Override
+            public void onFragmentDetached(@NonNull FragmentManager fm, @NonNull Fragment f) {
+                super.onFragmentDetached(fm, f);
+                if (f == fragment) {
                     AutoClearedValue.this.value = null;
-                    fragmentManager.unregisterFragmentLifecycleCallbacks(this);
+                    manager.unregisterFragmentLifecycleCallbacks(this);
                 }
             }
         }, false);
         this.value = value;
-    }
-
-    public T get() {
-        return this.value;
     }
 }
